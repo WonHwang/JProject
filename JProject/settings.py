ab202147e763aae87b8f35afe546a72472d0d9ec
Jeeweon Hwang <ediblepotato@gmail.com>
2024-05-12 22:52:07 +0900

    fix: remake project

--- JProject/settings.py
+++ JProject/settings.py
@@ -11,12 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 import json
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_FILE = 'JProject/secret.json'
 SECRETS = json.loads(open(SECRET_FILE).read())
 SECRET_KEY = SECRETS.get('SECRET_KEY')
